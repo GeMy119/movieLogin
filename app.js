167dc57e12f343d03b3752e48bb53c240ef6cd29
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-10-16 02:32:24 +0300

    movie login

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const connection = require("./connection/connectDB")
 const userRouter = require("./module/user/router/user.router")
+const core = require("cors")
 const app = express()
 const port = process.env.PORT || 5000;
 
+
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+app.use(cors(corsOptions));
 app.use(express.json())
 connection()
 app.use(userRouter)
