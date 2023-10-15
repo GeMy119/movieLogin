2ba3458450c4bf84f859b32e5a894a3d98d73f60
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-10-15 21:04:15 +0300

    movie login

--- connection/connectDB.js
+++ connection/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connection = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/movieUser").then((result) => {
+    mongoose.connect("mongodb+srv://movieLogin:movieLogin@atlascluster.zgoi85r.mongodb.net/").then((result) => {
         console.log("db Running")
     }).catch((err) => {
         console.log(err)
