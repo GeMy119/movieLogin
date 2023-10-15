fa4e54a2a1a4b0c763673ffd7542734c4b166450
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-10-15 20:45:15 +0300

    movie login

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express")
 const connection = require("./connection/connectDB")
 const userRouter = require("./module/user/router/user.router")
 const app = express()
-
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 connection()
 app.use(userRouter)
 
 
-app.listen(5000,()=>{
-    console.log("app running in port 5000")
+app.listen(port, () => {
+    console.log(`app running in port ${port}`)
 })
